b0d0509c8d68cf208952ae437eadb264f1418a55
Denis <denis_sergeev@epam.com>
2019-03-05 21:08:24 +0600

    commit of changes

--- CoreTest/CoreTest/Controllers/HomeController.cs
+++ CoreTest/CoreTest/Controllers/HomeController.cs
@@ -40,5 +40,12 @@ namespace CoreTest.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult DenisView()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
     }
 }
